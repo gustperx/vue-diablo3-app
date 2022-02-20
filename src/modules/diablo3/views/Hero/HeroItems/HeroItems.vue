08e9d626972b92b8c2408ab4d399d5d2ab7cdb8a
gustperx <gustperx@gmail.com>
2022-02-20 16:49:15 -0400

    empty item

--- src/modules/diablo3/views/Hero/HeroItems/HeroItems.vue
+++ src/modules/diablo3/views/Hero/HeroItems/HeroItems.vue
@@ -9,6 +9,7 @@
         <div></div>
         <div>
           <ItemDetail :item="items.head" v-if="items.head" />
+          <ItemDetailEmpty v-else item="head" />
         </div>
         <div></div>
       </div>
@@ -18,17 +19,33 @@
       <div class="my-4 grid grid-cols-3 gap-4">
         <div>
           <ItemDetail :item="items.shoulders" v-if="items.shoulders" />
+          <ItemDetailEmpty v-else item="shoulders" />
         </div>
-        <div><ItemDetail :item="items.torso" v-if="items.torso" /></div>
-        <div><ItemDetail :item="items.neck" v-if="items.neck" /></div>
+        <div>
+          <ItemDetail :item="items.torso" v-if="items.torso" />
+          <ItemDetailEmpty v-else item="torso" />
+        </div>
+        <div>
+          <ItemDetail :item="items.neck" v-if="items.neck" />
+          <ItemDetailEmpty v-else item="neck" />
+        </div>
       </div>
 
       <hr />
 
       <div class="my-4 grid grid-cols-3 gap-4">
-        <div><ItemDetail :item="items.hands" v-if="items.hands" /></div>
-        <div><ItemDetail :item="items.waist" v-if="items.waist" /></div>
-        <div><ItemDetail :item="items.bracers" v-if="items.bracers" /></div>
+        <div>
+          <ItemDetail :item="items.hands" v-if="items.hands" />
+          <ItemDetailEmpty v-else item="hands" />
+        </div>
+        <div>
+          <ItemDetail :item="items.waist" v-if="items.waist" />
+          <ItemDetailEmpty v-else item="waist" />
+        </div>
+        <div>
+          <ItemDetail :item="items.bracers" v-if="items.bracers" />
+          <ItemDetailEmpty v-else item="bracers" />
+        </div>
       </div>
 
       <hr />
@@ -36,10 +53,15 @@
       <div class="my-4 grid grid-cols-3 gap-4">
         <div>
           <ItemDetail :item="items.leftFinger" v-if="items.leftFinger" />
+          <ItemDetailEmpty v-else item="leftFinger" />
         </div>
-        <div><ItemDetail :item="items.legs" v-if="items.legs" /></div>
+        <div>
+          <ItemDetail :item="items.legs" v-if="items.legs" />
+          <ItemDetailEmpty v-else item="legs" />
+        </div>
         <div>
           <ItemDetail :item="items.rightFinger" v-if="items.rightFinger" />
+          <ItemDetailEmpty v-else item="rightFinger" />
         </div>
       </div>
 
@@ -48,9 +70,16 @@
       <div class="my-4 grid grid-cols-3 gap-4">
         <div>
           <ItemDetail :item="items.mainHand" v-if="items.mainHand" />
+          <ItemDetailEmpty v-else item="mainHand" />
         </div>
-        <div><ItemDetail :item="items.feet" v-if="items.feet" /></div>
-        <div><ItemDetail :item="items.offHand" v-if="items.offHand" /></div>
+        <div>
+          <ItemDetail :item="items.feet" v-if="items.feet" />
+          <ItemDetailEmpty v-else item="feet" />
+        </div>
+        <div>
+          <ItemDetail :item="items.offHand" v-if="items.offHand" />
+          <ItemDetailEmpty v-else item="offHand" />
+        </div>
       </div>
     </div>
   </section>
@@ -59,6 +88,7 @@
 <script lang="ts" setup>
 import ItemDetail from "./ItemDetail.vue";
 import type { HeroItems } from "@/modules/diablo3/interfaces/DetailedHeroItems";
+import ItemDetailEmpty from "./ItemDetailEmpty.vue";
 
 defineProps<{
   items: HeroItems;
---
+++ src/modules/diablo3/views/Hero/HeroItems/ItemDetailEmpty.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="d3-icon-item h-28 bg-gray-800 pt-3">
+    <div class="flex flex-col items-center justify-center">
+      <div class="mt-6 font-mono text-sm text-red-600">
+        {{ item }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  item: string;
+}>();
+</script>
+
+<style scoped>
+.d3-icon-item {
+  min-height: 100px;
+  border-top-style: solid;
+  border-top-width: 4px;
+}
+.d3-icon-item.item-white {
+  border-color: #a0aab5;
+}
+</style>
